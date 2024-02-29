ba19ce32a9ba6c554269c57740abfdca5653c417
leeyeonwoo <rexrex1105@gmail.com>
2024-02-29 23:39:04 +0900

    d

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-/**
+/* *
  * Sample React Native App
  * https://github.com/facebook/react-native
  *
@@ -29,32 +29,6 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
@@ -63,40 +37,25 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.sectionContainer}>
+      <View style={styles.subContainer}>
+        <Text>ㅎㅇ</Text>
+      </View>
+      <View style={styles.subContainer}>
+        <Text>헬로</Text>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  subContainer:{
+    flex: 2,
+  },
+  mainContainer:{
+    flex: 1,
+    fontSize: 32
+  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
